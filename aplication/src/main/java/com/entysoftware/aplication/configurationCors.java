96b056fa747fbf8187faa43a997651bf0e4675cf
andres <andresvelasquez182006@gmail.com>
2026-06-07 18:12:23 -0500

    configuracion CORS 0.1

--- aplication/src/main/java/com/entysoftware/aplication/configurationCors.java
+++ aplication/src/main/java/com/entysoftware/aplication/configurationCors.java
@@ -1,3 +1,4 @@
+package com.entysoftware.aplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
